b7f0dd470040dffa13a78fd72c7e3c79db7cea78
Brian Watkins <brian.watkins@gmail.com>
2022-03-26 14:38:11 -0400

    Print any browser logs during the test

--- behaviors/browser.ts
+++ behaviors/browser.ts
@@ -9,7 +9,15 @@ export async function startBrowser(): Promise<void> {
 }
 
 export async function newBrowserPage(): Promise<Page> {
-  return browser.newPage()
+  const page = await browser.newPage()
+  page.on("console", (message) => {
+    if (message.text().startsWith("[vite]")) {
+      return
+    }
+    console.log(message)
+  })
+  page.on("pageerror", console.log)
+  return page
 }
 
 export async function stopBrowser(): Promise<void> {
