a75104f9bf2a03c8a6304a6010477c4b6883a3ce
Brian Watkins <brian.watkins@gmail.com>
2022-03-26 07:31:27 -0400

    Fix paths in function build script

--- api/build.mjs
+++ api/build.mjs
@@ -2,13 +2,13 @@ import esbuild from "esbuild"
 
 await esbuild.build({
     entryPoints: [
-        "./api/backstage/index.ts"
+        "./backstage/index.ts"
     ],
     platform: "node",
     format: "esm",
     target: "node16",
     bundle: true,
-    outfile: "./api/build/backstage/index.mjs",
+    outfile: "./build/backstage/index.mjs",
     external: [
         "@azure/functions"
     ],
