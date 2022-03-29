1d2dce0aa0bfe1193ac1c37ee0a0f704ff0a1548
Brian Watkins <brian.watkins@gmail.com>
2022-03-29 06:53:04 -0400

    Add more learning areas to the static list

--- src/staticLearningAreasReader.ts
+++ src/staticLearningAreasReader.ts
@@ -2,9 +2,29 @@ import { LearningArea } from "./learningAreas.js";
 import { LearningAreasReader } from "./requestLearningAreas.js";
 
 export class StaticLearningAreasReader implements LearningAreasReader {
-    async read(): Promise<LearningArea[]> {
-        return [{
-            title: "Test-Driven Development"
-        }]
-    }
+  async read(): Promise<LearningArea[]> {
+    return [
+      {
+        title: "Test-Driven Development"
+      },
+      {
+        title: "Pair Programming Techniques"
+      },
+      {
+        title: "Mocking During Tests"
+      },
+      {
+        title: "Participate in a Retro"
+      },
+      {
+        title: "Participate in IPM"
+      },
+      {
+        title: "Explain the Principles and Values Behind Agile"
+      },
+      {
+        title: "Explain the Principles and Values Behind XP"
+      }
+    ]
+  }
 }
