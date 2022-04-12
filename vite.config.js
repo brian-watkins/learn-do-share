51fa8e4aa474e975474b925378e940de7ed3448e
Brian Watkins <brian.watkins@gmail.com>
2022-04-11 21:13:15 -0400

    Use vite plugin to copy index.html file during build
    - so that the root api function will have it to
    use as a template

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,45 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 /**
  * @type {import('vite').UserConfig}
  */
 const config = {
-    root: "./display",
-    server: {
-        port: 7663,
-        open: true,
-        proxy: {
-            "/api/backstage": "http://localhost:7664"
-        }
-    },
-    build: {
-        outDir: "../build/display",
-        emptyOutDir: true,
-        sourcemap: true
+  root: "./display",
+  server: {
+    port: 7663,
+    open: true,
+    proxy: {
+      "/api/backstage": "http://localhost:7664"
     }
+  },
+  build: {
+    outDir: "../build/display",
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  plugins: [
+    copyIndex("./api/root")
+  ]
+}
+
+function copyIndex(destinationDir) {
+  return {
+    name: 'copy-index',
+    enforce: 'post',
+    apply: 'build',
+    async writeBundle(options, bundle) {
+      const indexFile = bundle['index.html']
+      const outputFile = path.resolve(dirname(), destinationDir, "index.html")
+      fs.writeFileSync(outputFile, indexFile.source)
+      console.log("[copy-index] Wrote", outputFile)
+    }
+  }
+}
+
+function dirname() {
+  return path.dirname(fileURLToPath(import.meta.url))
 }
 
 export default config
