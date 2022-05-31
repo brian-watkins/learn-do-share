3eb4e72b8d639a8322c7a2a45c3fe20d6b321362
Brian Watkins <brian.watkins@gmail.com>
2022-05-31 19:23:26 -0400

    Run vite as a separate test process

--- behaviors/testServer.ts
+++ behaviors/testServer.ts
@@ -5,11 +5,15 @@ const funcPort = "7072"
 const cosmosPort = "3021"
 const serverPort = "4280"
 
+const VITE_SERVER = new TestProcess("npx", [
+  "vite", "--port", vitePort
+])
+
 const SWA_SERVER = new TestProcess("node_modules/.bin/swa", [
   "start", `http://localhost:${vitePort}`,
   "--api-location", `http://localhost:${funcPort}`,
   "--verbose", "log",
-  "--run", `npx vite --port ${vitePort}`
+  // "--run", `npx vite --port ${vitePort}`
 ])
 
 const FUNCTION_SERVER = new TestProcess("func", [
@@ -31,6 +35,10 @@ export async function startServer(): Promise<void> {
     logLevel: LogLevel.Normal
   })
 
+  VITE_SERVER.start({
+    logLevel: LogLevel.Normal
+  })
+
   SWA_SERVER.start({
     logLevel: LogLevel.Normal
   })
@@ -42,6 +50,8 @@ export async function stopServer(): Promise<void> {
   console.log("Stopping server")
   FUNCTION_SERVER.stop(StopSignal.Kill)
   console.log("Function server killed")
+  VITE_SERVER.stop()
+  console.log("Vite server killed")
   SWA_SERVER.stop()
   console.log("SWA Server killed")
 }
