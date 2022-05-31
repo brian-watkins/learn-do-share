cb76a35109adeb8574866981a2df86381e6fc076
Brian Watkins <brian.watkins@gmail.com>
2022-05-31 07:03:36 -0400

    Add logs to debug workflow

--- behaviors/testServer.ts
+++ behaviors/testServer.ts
@@ -8,7 +8,7 @@ const serverPort = "4280"
 const SWA_SERVER = new TestProcess("node_modules/.bin/swa", [
   "start", `http://localhost:${vitePort}`,
   "--api-location", `http://localhost:${funcPort}`,
-  "--verbose", "silent",
+  "--verbose", "log",
   "--run", `npx vite --port ${vitePort}`
 ])
 
@@ -28,11 +28,11 @@ export async function startServer(): Promise<void> {
     env: {
       COSMOS_DB_ENDPOINT: `https://localhost:${cosmosPort}`
     },
-    logLevel: LogLevel.Silent
+    logLevel: LogLevel.Normal
   })
 
   SWA_SERVER.start({
-    logLevel: LogLevel.Silent
+    logLevel: LogLevel.Normal
   })
 
   return waitForPort(serverPort)
