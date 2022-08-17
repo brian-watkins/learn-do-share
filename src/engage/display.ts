c429342e2ee40b9edf412569814c4927aebfbfd7
Brian Watkins <brian.watkins@gmail.com>
2022-08-17 06:01:01 -0400

    Broke out display functions to be easier to work with

--- src/engage/display.ts
+++ src/engage/display.ts
@@ -12,6 +12,7 @@ import { EngagementPlanMessages, engagementPlanWriteInProgress } from "./engagem
 import { EngagementNote } from "./engagementNotes"
 import { EngagementLevels, engagementLevelsRetrieved, engagementLevelsSaving } from "./engagementPlans"
 import { sendBackstage } from "@/api/backstage/adapter"
+import { MessageDispatcher } from "@/display/effect"
 
 export interface Informative {
   type: "informative"
@@ -98,46 +99,56 @@ function learningAreasLink(): Html.View {
 
 type Messages = EngagementNoteMessages | EngagementPlanMessages
 
-const display: DisplayConfig<Model, Messages> = {
-  view,
-  update: (model, message) => {
+function process(dispatch: MessageDispatcher, model: Model, message: Messages) {
+  switch (message.type) {
+    case "writeEngagementPlan":
+      sendBackstage(dispatch, model, message)
+      dispatch(engagementPlanWriteInProgress())
+      break
+    case "deleteEngagementPlans":
+    case "engagementNoteCreationRequested":
+    case "engagementNoteDeleteRequested":
+      sendBackstage(dispatch, model, message)
+      break
+  }
+}
+
+function update(model: Personalized, message: Messages) {
+  switch (message.type) {
+    case "engagementPlanWriteInProgress":
+      model.engagementLevels = engagementLevelsSaving(model.engagementLevels.levels)
+      break
+    case "engagementPlanPersisted":
+      const levels = model.engagementLevels.levels
+      levels.push(message.plan.level)
+      model.engagementLevels = engagementLevelsRetrieved(levels)
+      break
+    case "engagementPlansDeleted":
+      model.engagementLevels = engagementLevelsRetrieved([])
+      break
+    case "engagementNotePersisted":
+      model.engagementNotes.unshift(message.note)
+      break
+    case "engagementNoteDeleted":
+      model.engagementNotes = model.engagementNotes.filter(note => note.id !== message.note.id)
+      break
+  }
+}
+
+function onlyPersonalized(update: (model: Personalized, message: Messages) => void): (model: Model, message: Messages) => void {
+  return (model, message) => {
     if (model.type === "informative") {
       return
     }
-
-    switch (message.type) {
-      case "engagementPlanWriteInProgress":
-        model.engagementLevels = engagementLevelsSaving(model.engagementLevels.levels)
-        break
-      case "engagementPlanPersisted":
-        const levels = model.engagementLevels.levels
-        levels.push(message.plan.level)
-        model.engagementLevels = engagementLevelsRetrieved(levels)
-        break
-      case "engagementPlansDeleted":
-        model.engagementLevels = engagementLevelsRetrieved([])
-        break
-      case "engagementNotePersisted":
-        model.engagementNotes.unshift(message.note)
-        break
-      case "engagementNoteDeleted":
-        model.engagementNotes = model.engagementNotes.filter(note => note.id !== message.note.id)
-        break
-    }
-  },
-  process: (dispatch, state, message) => {
-    switch (message.type) {
-      case "writeEngagementPlan":
-        sendBackstage(dispatch, state, message)
-        dispatch(engagementPlanWriteInProgress())
-        break
-      case "deleteEngagementPlans":
-      case "engagementNoteCreationRequested":
-      case "engagementNoteDeleteRequested":
-        sendBackstage(dispatch, state, message)
-        break
-    }
+  
+    update(model, message)
   }
+}
+
+const display: DisplayConfig<Model, Messages> = {
+  view,
+  update: onlyPersonalized(update),
+  process
 }
 
 export default display
