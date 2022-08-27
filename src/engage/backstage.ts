6a4b3e691ca2b5199a4b060ecaf28405c9103f35
Brian Watkins <brian.watkins@gmail.com>
2022-08-27 17:55:32 -0400

    Return correct value from backstage

--- src/engage/backstage.ts
+++ src/engage/backstage.ts
@@ -10,7 +10,7 @@ import { noteContentTagStyles } from "./engagementNotes/view.js";
 import { LearningArea } from "./learningArea.js";
 import { EngagementNote, EngagementNoteContents, engagementNotesRetrieved } from "./engagementNotes";
 import { engagementLevelsRetrieved, EngagementPlan } from "./engagementPlans/index.js";
-import { EngagementNoteCreationRequested, EngagementNoteDeleteRequested, engagementNotePersisted } from "./engagementNotes/writeEngagementNote.js";
+import { EngagementNoteCreationRequested, EngagementNoteDeleteRequested } from "./engagementNotes/writeEngagementNote.js";
 
 export interface EngagementPlanReader {
   read(user: User): Promise<Array<EngagementPlan>>
@@ -53,7 +53,7 @@ const update = (adapters: Adapters) => async (user: User | null, message: DataMe
       return engagementPlansDeleted(message.learningArea)
     case "engagementNoteCreationRequested":
       const note = await adapters.engagementNoteWriter.write(user, message.learningAreaId, message.contents)
-      return engagementNotePersisted(displayableNote(note))
+      return displayableNote(note)
     case "engagementNoteDeleteRequested":
       await adapters.engagementNoteWriter.delete(user, message.note)
       return message.note
